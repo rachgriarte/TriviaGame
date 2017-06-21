bfcc71c9a1e3cec15bad3db982c3eddb9b8c3954
Rach Griarte <rachelgriarte@hotmail.com>
2017-06-20 20:58:56 -0400

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,21 @@
+alert ("Welcome to Rach's Vogue Trivia! Once you click 'OK' you have 1 minute to answer all 10 trivia questions! Good luck :) " )
+
 //timer
 function countDown (secs, elem) {
 	var element=document.getElementById(elem);
-	element.innerHTML = "You have "+secs+" seconds to answer 10 Vogue questions!";
+	element.innerHTML = secs+" seconds!";
 	if(secs<1) {
 		clearTimeout(timer);
-		element.innerHTML = '<h2>Time is up! Check your score!</h2>';
+		element.innerHTML = '<h4>Time is up! Check your score!</h4>';
 	}
 	secs--;
 	var timer=setTimeout('countDown('+secs+',"'+elem+'")',1000);
 }
 
-//Questions, Choices, Answers, Images
+//Questions, Choices, Answers
 function check(){
 	console.log("hi");
+
 var question1=document.quiz.question1.value;
 var question2=document.quiz.question2.value;
 var question3=document.quiz.question3.value;
@@ -27,72 +30,71 @@ var correct=0;
 var incorrect=0;
 
 // If/else for correct and incorrect
-if (question1 == "Anna Wintour") {
-	correct++;
-	range=0;
-} else {
-	incorrect++
-}
+	if (question1 == "Anna Wintour") {
+		correct++;
+	} else {
+		incorrect++
+	}
 
-if (question2 == "1892") {
-	correct++;
-} else {
-	incorrect++
-}
+	if (question2 == "1892") {
+		correct++;
+	} else {
+		incorrect++
+	}
 
-if (question3 == "Arthur Baldwin") {
-	correct++;
-} else {
-	incorrect++
-}
+	if (question3 == "Arthur Baldwin") {
+		correct++;
+	} else {
+		incorrect++
+	}
 
-if (question4 == "Conde Nast") {
-	correct++;
-} else {
-	incorrect++
-}
+	if (question4 == "Conde Nast") {
+		correct++;
+	} else {
+		incorrect++
+	}
 
-if (question5 == "17 countries") {
-	correct++;
-} else {
-	incorrect++
-}
+	if (question5 == "17 countries") {
+		correct++;
+	} else {
+		incorrect++
+	}
 
-if (question6 == "Zendaya") {
-	correct++;
-} else {
-	incorrect++
-}
+	if (question6 == "Zendaya") {
+		correct++;
+	} else {
+		incorrect++
+	}
 
-if (question7 == "British Vogue") {
-	correct++;
-} else {
-	incorrect++
-}
+	if (question7 == "British Vogue") {
+		correct++;
+	} else {
+		incorrect++
+	}
 
-if (question8 == "Beverly Johnson") {
-	correct++;
-} else {
-	incorrect++
-}
+	if (question8 == "Beverly Johnson") {
+		correct++;
+	} else {
+		incorrect++
+	}
 
-if (question9 == "To influence American fashion and lifestyle") {
-	correct++;
-} else {
-	incorrect++
-}
+	if (question9 == "To influence American fashion and lifestyle") {
+		correct++;
+	} else {
+		incorrect++
+	}
 
-if (question10 == "Madonna") {
-	correct++;
-} else {
-	incorrect++
-}
+	if (question10 == "Madonna") {
+		correct++;
+	} else {
+		incorrect++
+
+	}
 
 document.getElementById("after_submit").style.visibility ="visible";
 document.getElementById("number_correct").innerHTML = "You got " + correct+ " correct!";
-document.getElementById("number_incorrect").innerHTML = "You got " + incorrect+ " incorrect :( ";
+document.getElementById("number_incorrect").innerHTML = "You got " + incorrect+ " incorrect.";
 }
 
-
-
-
+//Reset
+function reset(){}
